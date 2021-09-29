835fec75c4e4684788750a2ad688320c25f5d38d
ZekeMoore <88862310+ZekeMoore@users.noreply.github.com>
2021-09-29 13:37:05 -0400

    Update Employee_Challenge_starter_code.sql

--- Pewlett-Hackard-Analysis Folder/Employee_Challenge_starter_code.sql
+++ Pewlett-Hackard-Analysis Folder/Employee_Challenge_starter_code.sql
@@ -1,9 +1,52 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+--Creating Rerirement Titles table
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ttl.title,
+	ttl.from_date,
+	ttl.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ttl
+	ON (e.emp_no = ttl.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31');
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+-- Creating Retiring Titles table
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+
+-- Creating Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) 
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ttl.title
+-- INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+	ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ttl
+	ON (e.emp_no = ttl.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
