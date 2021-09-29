deb62eeaa07f26e73d064bacb43b603997eb932a
ZekeMoore <88862310+ZekeMoore@users.noreply.github.com>
2021-09-29 13:47:15 -0400

    Update schema.sql

--- Pewlett-Hackard-Analysis Folder/schema.sql
+++ Pewlett-Hackard-Analysis Folder/schema.sql
@@ -1,128 +1,61 @@
-Departments
--
-dept_no varchar pk 
-dept_name varchar
-
-Employees
--
-emp_no int FK >- Dept_Emp.emp_no
-birth_date date
-first_name varchar
-last_name varchar
-gender varchar
-hire_date date
-
-Salaries
--
-emp_no int FK >- Employees.emp_no
-salary int
-from_date date
-to_date date
-
-Titles
--
-emp_no int FK >- Employees.emp_no
-title varchar
-from_date date
-to_date date
-
-Managers
--
-dept_no varchar pk fk - Departments.dept_no
-emp_no int pk fk - Employees.emp_no
-from_date date
-to_date date
-
-Dept_Emp
--
-dept_no FK >- Departments.dept_no
-emp_no FK >- Salaries.emp_no
-from_date
-to_date
-
-
-CREATE TABLE titles (
- emp_no INT NOT NULL,
- title VARCHAR NOT NULL,
- from_date DATE NOT NULL,
- to_date DATE NOT NULL,
- FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
- PRIMARY KEY (emp_no, title, from_date)
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments(
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
 );
 
-CREATE TABLE department_employees (
- emp_no INT NOT NULL,
- dept_no VARCHAR(4) NOT NULL,
- from_date DATE NOT NULL,
- to_date DATE NOT NULL, 
- FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
- PRIMARY KEY (emp_no, dept_no, from_date)
+CREATE TABLE employees(
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM department_employees;
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
 
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
 
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
 
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
-
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
-
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
-
--- Retirement eligibility
-SELECT first_name, last_name
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
--- Number of employees retiring
-SELECT COUNT(first_name)
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT first_name, last_name
-INTO retirement_info
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+CREATE TABLE titles(
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
 
 
--- Create new table for retiring employees
-SELECT emp_no, first_name, last_name
-INTO retirement_info
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
--- Check the table
-SELECT * FROM retirement_info;
+SELECT * FROM dept_emp;
 
--- Joining departments and dept_manager tables
-SELECT departments.dept_name,
-     dept_manager.emp_no,
-     dept_manager.from_date,
-     dept_manager.to_date
-FROM departments
-INNER JOIN dept_manager
-ON departments.dept_no = dept_manager.dept_no;
+DROP TABLE titles;
 
--- Joining retirement_info and dept_emp tables
-SELECT retirement_info.emp_no,
-    retirement_info.first_name,
-retirement_info.last_name,
-    dept_emp.to_date
-FROM retirement_info
-LEFT JOIN department_employees
-ON retirement_info.emp_no = dept_emp.emp_no;
+DROP TABLE dept_emp;
